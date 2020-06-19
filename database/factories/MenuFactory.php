bbb0de0d96d29036dd2fda9610e840ee02954fd3
Ilyas Bendaas <ilyesben93@live.fr>
2020-06-19 16:37:34 +0100

    working on Menu
    Model, Migration and Factory setup

---
+++ database/factories/MenuFactory.php
@@ -0,0 +1,17 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Menu;
+use Faker\Generator as Faker;
+
+$factory->define(App\Models\Menu::class, function (Faker $faker) {
+    return [
+        //
+        
+        'name' => $faker -> word,
+        'description' => $faker ->paragraphs(2, true),
+        'price'=> $faker ->numberBetween(99, 999),
+        
+    ];
+});
