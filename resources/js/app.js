ff36b707cad116d4d8b0a4e9a3be593950ab3e9d
Ilyas Bendaas <ilyesben93@live.fr>
2020-06-21 12:27:13 +0100

    Vuejs slots for re-useable Card component

--- resources/js/app.js
+++ resources/js/app.js
@@ -19,7 +19,14 @@ window.Vue = require('vue');
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+/**
+ * These are the global components
+ */
+
+Vue.component('card-component', () => import ('./components/Card.vue'));
+
+
+
 Vue.component('menu-container', () => import ('./modules/menu/MenuContainer.vue'));
 
 /**
--- resources/js/modules/menu/MenuContainer.vue
+++ resources/js/modules/menu/MenuContainer.vue
@@ -1,6 +1,20 @@
 <template>
     <div class="wrapper menu__container">
-        <p>This is the menu container</p>
+        <div class="row">
+            <div class="col-md-8">
+                <card-component>
+                    <template slot="title">my Menu items</template>
+                    <template slot="body">Content will come here</template>
+                </card-component>
+            </div>
+            
+            <div class="col-md-4">
+                <card-component>
+                    <template slot="title">Add Menu items</template>
+                    <template slot="body">From will come here</template>
+                </card-component>
+            </div>
+        </div>
     </div>
 </template>
     
