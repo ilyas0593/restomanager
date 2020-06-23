9529e2cebec63762d08ed148b6aa3ee797ef3b8f
Ilyas Bendaas <ilyesben93@live.fr>
2020-06-23 14:30:40 +0100

    Fix App\Model files

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,9 +2,13 @@
 
 namespace App\Models;
 
+use App\Models\Menu;
 use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    public function menus()
+    {
+        return $this->hasMany(Menu::class);
+    }
 }
