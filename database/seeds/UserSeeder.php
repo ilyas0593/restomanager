d1b280228f8df8685ce4ffb174b17a807cdeb00c
Ilyas Bendaas <ilyesben93@live.fr>
2020-06-23 15:59:02 +0100

    Eloquent relationship for Restaurant and order by in relationship

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -13,10 +13,25 @@ class UserSeeder extends Seeder
     {
         //
 
-        \App\User::create([
+        $user = \App\User::create([
             'name' => 'Ilyas Bendaas',
             'email' => 'ilyesben93@live.fr',
             'password' => bcrypt('password'),
+        ]);
+
+        $user->restaurants()->create([
+            'name' => 'Highway Star',
+            'location' => 'Bhuvandas Road, NH14 Dadar, Mumbai',
+        ]);
+
+        $user->restaurants()->create([
+            'name' => 'Mainland China',
+            'location' => 'Platinum Tech Park Vashi, Navi Mumbai',
+        ]);
+
+        $user->restaurants()->create([
+            'name' => 'Mukesh Lunch Home',
+            'location' => 'Sai Balaji Tower, Airoli Navi Mumbai',
         ]);
 
     }
