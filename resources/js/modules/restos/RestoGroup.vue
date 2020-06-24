240a52fb65ecd349986cfac6de4af2afa2c0d1e4
Ilyas Bendaas <ilyesben93@live.fr>
2020-06-24 10:48:33 +0100

    Modal popup to add restaurant form

--- resources/js/modules/restos/RestoGroup.vue
+++ resources/js/modules/restos/RestoGroup.vue
@@ -10,15 +10,29 @@
       <div class="col-md-4" v-if="showAddForm">
         <card-component>
           <template slot="title">Add new Restaurant</template>
-          <template slot="main">+</template>
+          <template slot="main">
+            <span @click="handleAddNewResto">+</span>
+          </template>
         </card-component>
+        <modal name="add-new-resto" height="50%">
+          <div class="container-padding">
+            <resto-add-form 
+            @addRestoEvent="handleSaveResto"
+            @modalCancel="handleCancelResto"></resto-add-form>
+          </div>
+        </modal>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import RestoAddForm from './RestoAddForm.vue';
+
 export default {
+  components: {
+    RestoAddForm
+  },
   props: ['restos'],
   created() {
     console.log('this.restos.length', this.restos.length);
@@ -30,6 +44,17 @@ export default {
   },
   data() {
     return {}
+  },
+  methods: {
+    handleAddNewResto(){
+      this.$modal.show('add-new-resto');
+    },
+    handleCancelResto(){
+      console.log('i was clicked');
+    },
+    handleSaveResto(restoData){
+      console.log('restodata', restoData);
+    }
   }
 }
 </script>
